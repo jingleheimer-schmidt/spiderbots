f99897115a4cf4d007308128d14fa4dad49e4646
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2023-11-26 13:15:46 -0800

    rename to backpack mode toggle

--- util/commands.lua
+++ util/commands.lua
@@ -1,6 +1,7 @@
 
 local tasks_util = require("util/tasks")
 local abandon_task = tasks_util.abandon_task
+local return_to_inventory = tasks_util.return_to_inventory
 
 local function toggle_debug()
 	global.debug = not global.debug
@@ -11,13 +12,13 @@ local function toggle_debug()
 end
 
 ---@param event EventData.on_console_command
-local function recall_spiderbots(event)
+local function toggle_backpack(event)
 	local player_index = event.player_index
 	if not player_index then return end
 	local player = game.get_player(player_index)
 	if not player then return end
 
-	-- abandon all tasks
+	-- abandon tasks and return to the player's inventory
 	global.spiders[player_index] = global.spiders[player_index] or {}
 	for spider_id, spider in pairs(global.spiders[player_index]) do
 		if spider.valid then
@@ -26,13 +27,27 @@ local function recall_spiderbots(event)
 			global.spiders[player_index][spider_id] = nil
 		end
 	end
+	for surface_index, available_spiders in pairs(global.available_spiders[player_index]) do
+		for spider_index, spider in pairs(available_spiders) do
+			if spider.valid then
+				local spider_surface_index = spider.surface_index
+				local player_surface_index = player.surface_index
+				if spider_surface_index == player_surface_index then
+					return_to_inventory(spider, player)
+					table.remove(available_spiders, spider_index)
+				end
+			else
+				table.remove(available_spiders, spider_index)
+			end
+		end
+	end
 end
 
 local function add_commands()
-	commands.add_command("spiderbots-debug",
+	commands.add_command("debug-spiderbots",
 		"- toggles debug mode for the spiderbots, showing task targets and path request renderings", toggle_debug)
-	commands.add_command("spiderbots-recall",
-		"- recalls all spiderbots to the player's inventory", recall_spiderbots)
+	commands.add_command("spiderbots-backpack",
+		"- toggles backpack mode for the spiderbots, actively recalls all spiderbots to the player's inventory", toggle_backpack)
 end
 
 return {
