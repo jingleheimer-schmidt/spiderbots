a310f204fc693c9249e97afc8f5fdad1e9ad3fa0
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2023-11-26 10:54:52 -0800

    add recall_spiderbots command

--- util/commands.lua
+++ util/commands.lua
@@ -10,9 +10,29 @@ local function toggle_debug()
 	end
 end
 
+---@param event EventData.on_console_command
+local function recall_spiderbots(event)
+	local player_index = event.player_index
+	if not player_index then return end
+	local player = game.get_player(player_index)
+	if not player then return end
+
+	-- abandon all tasks
+	global.spiders[player_index] = global.spiders[player_index] or {}
+	for spider_id, spider in pairs(global.spiders[player_index]) do
+		if spider.valid then
+			abandon_task(spider, player)
+		else
+			global.spiders[player_index][spider_id] = nil
+		end
+	end
+end
+
 local function add_commands()
 	commands.add_command("spiderbots-debug",
 		"- toggles debug mode for the spiderbots, showing task targets and path request renderings", toggle_debug)
+	commands.add_command("spiderbots-recall",
+		"- recalls all spiderbots to the player's inventory", recall_spiderbots)
 end
 
 return {
