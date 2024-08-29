7ae1d2c613ea555eec744268eb2f9150726999db
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-08-28 22:39:54 -0700

    standardize formatting

--- data.lua
+++ data.lua
@@ -3,11 +3,11 @@ local sound_probability = 0.125
 local sound_volume = 1
 
 local spiderbot_arguments = {
-	scale = 0.25,
-	leg_scale = 0.82,
-	name = "spiderbot",
-	leg_thickness = 1.44,
-	leg_movement_speed = 2.5,
+    scale = 0.25,
+    leg_scale = 0.82,
+    name = "spiderbot",
+    leg_thickness = 1.44,
+    leg_movement_speed = 2.5,
 }
 create_spidertron(spiderbot_arguments)
 local spiderbot_prototype = data.raw["spider-vehicle"]["spiderbot"]
@@ -26,47 +26,47 @@ spiderbot_prototype.working_sound.probability = 1 / 4
 spiderbot_prototype.minimap_representation.scale = 0.125
 local legs = spiderbot_prototype.spider_engine.legs
 if legs[1] then
-	for _, leg in pairs(legs) do
-		for _, trigger in pairs(leg.leg_hit_the_ground_trigger) do
-			trigger.repeat_count = 1
-			trigger.probability = 1 / 32
-		end
-		local leg_name = leg.leg
-		local leg_prototype = data.raw["spider-leg"][leg_name]
-		leg_prototype.walking_sound_volume_modifier = 0
-		leg_prototype.working_sound.probability = sound_probability
-	end
+    for _, leg in pairs(legs) do
+        for _, trigger in pairs(leg.leg_hit_the_ground_trigger) do
+            trigger.repeat_count = 1
+            trigger.probability = 1 / 32
+        end
+        local leg_name = leg.leg
+        local leg_prototype = data.raw["spider-leg"][leg_name]
+        leg_prototype.walking_sound_volume_modifier = 0
+        leg_prototype.working_sound.probability = sound_probability
+    end
 else
-	for _, trigger in pairs(legs.leg_hit_the_ground_trigger) do
-		trigger.repeat_count = 1
-		trigger.probability = 1 / 32
-	end
-	local leg_name = legs.leg
-	local leg_prototype = data.raw["spider-leg"][leg_name]
-	leg_prototype.walking_sound_volume_modifier = 0
-	leg_prototype.working_sound.probability = sound_probability
+    for _, trigger in pairs(legs.leg_hit_the_ground_trigger) do
+        trigger.repeat_count = 1
+        trigger.probability = 1 / 32
+    end
+    local leg_name = legs.leg
+    local leg_prototype = data.raw["spider-leg"][leg_name]
+    leg_prototype.walking_sound_volume_modifier = 0
+    leg_prototype.working_sound.probability = sound_probability
 end
 local selection_box = spiderbot_prototype.selection_box
 if selection_box then
-	selection_box[1][1] = selection_box[1][1] * 2
-	selection_box[1][2] = selection_box[1][2] * 2
-	selection_box[2][1] = selection_box[2][1] * 2
-	selection_box[2][2] = selection_box[2][2] * 2
+    selection_box[1][1] = selection_box[1][1] * 2
+    selection_box[1][2] = selection_box[1][2] * 2
+    selection_box[2][1] = selection_box[2][1] * 2
+    selection_box[2][2] = selection_box[2][2] * 2
 end
 data:extend { spiderbot_prototype }
 
 for i = 1, 8 do
-	local leg = data.raw["spider-leg"]["spiderbot-leg-" .. i]
-	leg.collision_mask = { "object-layer", "water-tile", "rail-layer", "not-colliding-with-itself" }
+    local leg = data.raw["spider-leg"]["spiderbot-leg-" .. i]
+    leg.collision_mask = { "object-layer", "water-tile", "rail-layer", "not-colliding-with-itself" }
 end
 
 local spiderbot_recipe = table.deepcopy(data.raw["recipe"]["spidertron"])
 spiderbot_recipe.name = "spiderbot"
 spiderbot_recipe.ingredients = {
-	{ "electronic-circuit", 4 },
-	{ "iron-plate",         12 },
-	{ "inserter",           8 },
-	{ "raw-fish",           1 },
+    { "electronic-circuit", 4 },
+    { "iron-plate",         12 },
+    { "inserter",           8 },
+    { "raw-fish",           1 },
 }
 spiderbot_recipe.result = "spiderbot-item"
 spiderbot_recipe.enabled = true
@@ -85,104 +85,104 @@ data:extend { spiderbot_recipe }
 
 local spidertron_item = table.deepcopy(data.raw["item-with-entity-data"]["spidertron"])
 local spiderbot_item = {
-	type = "capsule",
-	name = "spiderbot-item",
-	icon = spidertron_item.icon,
-	icon_size = spidertron_item.icon_size,
-	stack_size = 25,
-	subgroup = "logistic-network",
-	order = "a[robot]-a[little-spiderbot]",
-	capsule_action = {
-		type = "throw",
-		attack_parameters = {
-			activation_type = "throw",
-			ammo_category = "capsule",
-			type = "projectile",
-			cooldown = 7,
-			projectile_creation_distance = .3,
-			range = 25,
-			ammo_type = {
-				category = "capsule",
-				target_type = "position",
-			-- 	action = {
-			-- 		{
-			-- 			type = "direct",
-			-- 			action_delivery = {
-			-- 				type = "projectile",
-			-- 				projectile = "spiderbot-projectile",
-			-- 				starting_speed = 0.33,
-			-- 			}
-			-- 		}
-			-- 	}
-			}
-		}
-	}
+    type = "capsule",
+    name = "spiderbot-item",
+    icon = spidertron_item.icon,
+    icon_size = spidertron_item.icon_size,
+    stack_size = 25,
+    subgroup = "logistic-network",
+    order = "a[robot]-a[little-spiderbot]",
+    capsule_action = {
+        type = "throw",
+        attack_parameters = {
+            activation_type = "throw",
+            ammo_category = "capsule",
+            type = "projectile",
+            cooldown = 7,
+            projectile_creation_distance = .3,
+            range = 25,
+            ammo_type = {
+                category = "capsule",
+                target_type = "position",
+                -- 	action = {
+                -- 		{
+                -- 			type = "direct",
+                -- 			action_delivery = {
+                -- 				type = "projectile",
+                -- 				projectile = "spiderbot-projectile",
+                -- 				starting_speed = 0.33,
+                -- 			}
+                -- 		}
+                -- 	}
+            }
+        }
+    }
 }
 data:extend { spiderbot_item }
 
 local spiderbot_projectile = {
-	type = "projectile",
-	name = "spiderbot-projectile",
-	acceleration = 0.005,
-	action = {
-		action_delivery = {
-			target_effects = {
-				{
-					entity_name = "spiderbot",
-					type = "create-entity",
-					show_in_tooltip = true,
-					trigger_created_entity = "true"
-				}
-			},
-			type = "instant"
-		},
-		type = "direct"
-	},
-	animation = data.raw["projectile"]["distractor-capsule"].animation,
-	shadow = data.raw["projectile"]["distractor-capsule"].shadow,
-	flags = { "not-on-map" },
-	enable_drawing_with_mask = true,
+    type = "projectile",
+    name = "spiderbot-projectile",
+    acceleration = 0.005,
+    action = {
+        action_delivery = {
+            target_effects = {
+                {
+                    entity_name = "spiderbot",
+                    type = "create-entity",
+                    show_in_tooltip = true,
+                    trigger_created_entity = "true"
+                }
+            },
+            type = "instant"
+        },
+        type = "direct"
+    },
+    animation = data.raw["projectile"]["distractor-capsule"].animation,
+    shadow = data.raw["projectile"]["distractor-capsule"].shadow,
+    flags = { "not-on-map" },
+    enable_drawing_with_mask = true,
 }
 data:extend { spiderbot_projectile }
 
 local toggle_spiderbots_shortcut = {
-	type = "shortcut",
-	name = "toggle-spiderbots",
-	action = "lua",
-	associated_control_input = "toggle-spiderbots",
-	-- icon = {filename = "__spiderbots__/assets/targeted.png", size = 1024},
-	icon = { filename = "__spiderbots__/assets/icons8-spider-67.png", size = 67 },
-	-- disabled_icon = {filename = "__spiderbots__/assets/spider-face-trans.png", size = 1024},
-	toggleable = true,
+    type = "shortcut",
+    name = "toggle-spiderbots",
+    action = "lua",
+    associated_control_input = "toggle-spiderbots",
+    -- icon = {filename = "__spiderbots__/assets/targeted.png", size = 1024},
+    icon = { filename = "__spiderbots__/assets/icons8-spider-67.png", size = 67 },
+    -- disabled_icon = {filename = "__spiderbots__/assets/spider-face-trans.png", size = 1024},
+    toggleable = true,
 }
 data:extend({ toggle_spiderbots_shortcut })
 
 local toggle_spiderbots_hotkey = {
-	type = "custom-input",
-	name = "toggle-spiderbots",
-	key_sequence = "ALT + S",
-	-- alternative_key_sequence = "COMMAND + S",
-	action = "lua",
+    type = "custom-input",
+    name = "toggle-spiderbots",
+    key_sequence = "ALT + S",
+    -- alternative_key_sequence = "COMMAND + S",
+    action = "lua",
 }
 data:extend({ toggle_spiderbots_hotkey })
 
 local toggle_backpack_mode = {
-	type = "shortcut",
-	name = "toggle-backpack-mode",
-	action = "lua",
-	associated_control_input = "toggle-backpack-mode",
-	-- icon = {filename = "__spiderbots__/assets/targeted.png", size = 1024},
-	icon = {filename = "__spiderbots__/assets/icons8-spider-67.png", size = 67},
-	-- disabled_icon = {filename = "__spiderbots__/assets/spider-face-trans.png", size = 1024},
-	toggleable = true,
+    type = "shortcut",
+    name = "toggle-backpack-mode",
+    action = "lua",
+    associated_control_input = "toggle-backpack-mode",
+    -- icon = {filename = "__spiderbots__/assets/targeted.png", size = 1024},
+    icon = { filename = "__spiderbots__/assets/icons8-spider-67.png", size = 67 },
+    -- disabled_icon = {filename = "__spiderbots__/assets/spider-face-trans.png", size = 1024},
+    toggleable = true,
 }
-data:extend({toggle_backpack_mode})
+data:extend({ toggle_backpack_mode })
 
 local toggle_backpack_mode_hotkey = {
-	type = "custom-input",
-	name = "toggle-backpack-mode",
-	key_sequence = "SHIFT + ALT + S",
-	-- alternative_key_sequence = "COMMAND + S",
-	action = "lua",
+    type = "custom-input",
+    name = "toggle-backpack-mode",
+    key_sequence = "SHIFT + ALT + S",
+    -- alternative_key_sequence = "COMMAND + S",
+    action = "lua",
 }
-data:extend({toggle_backpack_mode_hotkey})
+data:extend({ toggle_backpack_mode_hotkey })
