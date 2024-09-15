816d21b67f3d5fdf1465f546a8eb981769e903be
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-09-15 11:01:10 -0700

    spider tasks are now immediately canceled when work order processing is toggled off

--- control.lua
+++ control.lua
@@ -1365,6 +1365,17 @@ local function toggle_spiderbots(event)
     if name ~= "toggle-spiderbots" then return end
     local player_index = event.player_index
     global.spiders_enabled[player_index] = not global.spiders_enabled[player_index]
+    if not global.spiders_enabled[player_index] then
+        local player = game.get_player(player_index)
+        local player_spiders = global.spiders[player_index]
+        if player and player.valid and player_spiders then
+            for spider_id, spider in pairs(player_spiders) do
+                if spider.valid then
+                    abandon_task(spider, player)
+                end
+            end
+        end
+    end
     game.get_player(player_index).set_shortcut_toggled("toggle-spiderbots", global.spiders_enabled[player_index])
 end
 
