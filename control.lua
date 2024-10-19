23cfd944e7774e505b122bd93b814e412a4d639a
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-10-18 18:42:08 -0700

    toggle_spiderbots

--- control.lua
+++ control.lua
@@ -1,1 +1,26 @@
 
+-- toggle the spiderbots on/off for the player
+---@param event EventData.on_lua_shortcut | EventData.CustomInputEvent
+local function toggle_spiderbots(event)
+    local name = event.prototype_name or event.input_name
+    if name ~= "toggle-spiderbots" then return end
+    local player_index = event.player_index
+    global.spiderbots_enabled = global.spiderbots_enabled or {}
+    global.spiderbots_enabled[player_index] = not global.spiderbots_enabled[player_index]
+    if not global.spiderbots_enabled[player_index] then
+        local player = game.get_player(player_index)
+        global.spiderbots = global.spiderbots or {}
+        local player_spiders = global.spiderbots[player_index]
+        if player and player.valid and player_spiders then
+            for spider_id, spider in pairs(player_spiders) do
+                if spider.valid then
+                    abandon_task(spider, player)
+                end
+            end
+        end
+    end
+    game.get_player(player_index).set_shortcut_toggled("toggle-spiderbots", global.spiderbots_enabled[player_index])
+end
+
+script.on_event("toggle-spiderbots", toggle_spiderbots)
+script.on_event(defines.events.on_lua_shortcut, toggle_spiderbots)
